3f96abc2423bfdf794d5c503273195e5af1d9f4b
Cruvadio <ialexandrova53@gmail.com>
2019-03-22 01:20:21 +0300

    Test

--- matrixes/main.cpp
+++ matrixes/main.cpp
@@ -7,9 +7,20 @@ int main()
     try
     {
         Rational_number rat("135", "573");
+        Vector vec(10, Ones);
+
+        vec(0)++;
+        vec(5) = rat;
+
+        vec(2) = rat*2;
+
+        vec.write("vector.txt");
+
+        char* str = vec.to_string();
+
+        printf("%s\n", str);
         
-        rat = rat / Rational_number();
-        printf("%s\n", rat.to_string());
+        delete[] str;
     }
     catch(NotARational &n)
     {
