e58b2271d64e41022b7c83855e635385cc2006fc
Cruvadio <ialexandrova53@gmail.com>
2019-03-24 02:25:57 +0300

    Test

--- matrixes/main.cpp
+++ matrixes/main.cpp
@@ -7,6 +7,7 @@ int main()
     try
     {
         Rational_number rat("135", "573");
+        Rational_number rat1 (-20);
         Vector vec(10, Zeros);
 
         ++vec(0);
@@ -15,20 +16,23 @@ int main()
 
         vec(2) = rat*2;
         
-        Vector vec2 = vec * (Rational_number)2;
+       // Vector vec2 = vec * (Rational_number)2;
         vec.write("vector.txt");
         
         vec(2) = 0;
         
         vec(3) = Rational_number(55, 13);
-        vec(8) = -30;
+        vec(8) = -30 + rat;
+        vec(2) -= rat1;
 
-        vec2 = vec + vec2;
-        vec = vec * (Rational_number)2;
+       // vec2 = vec + vec2;
+
+        vec.write("vector.txt");
+        //vec = vec * (Rational_number)2;
         char* str = vec.to_string();
 
         printf("%s\n", str);
-        vec2.write("vector2.txt");
+        //vec2.write("vector2.txt");
         delete[] str;
     }
     catch(NotARational &n)
